6c2033054539332959e5d7204fcf7ae5ce114d71
YEDENDRA B. SHRINIVASAN <yshrini@us.ibm.com>
2016-07-06 13:31:25 -0400

    removed config test

--- tests/testCelioConfig.js
+++
@@ -1,5 +0,0 @@
-const CELIO = require('../src/index.js');
-const io = new CELIO();
-console.log(io.config.get('sys'))
-// bound to see a connection error due to fake rabbit mq url in cog.json
-io.getTranscript();
